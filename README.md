6e7a11435cc4e107df713793da127ee32d51eadd
José Veríssimo <jose@koodoo.io>
2022-08-19 11:28:27 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## k4o-bumper
+Tiny package to bump the version on our services.
+
+### Installing
+```
+npm i -g k4o-bumper
+```
+
+
+### Examples
+```
+k4o-bumper --patch
+k4o-bumper --minor
+k4o-bumper --major
+k4o-bumper version=1.2.3
+```
---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+const semver = require('semver');
+const fs = require('fs');
+const package = require('./package.json');
+
+const getVersion = () => {
+  const patchArg = process.argv.slice(2).find(arg => arg.startsWith('--patch'));
+  const minorArg = process.argv.slice(2).find(arg => arg.startsWith('--minor'));
+  const majorArg = process.argv.slice(2).find(arg => arg.startsWith('--major'));
+  const versionSpecifiedArg = process.argv.slice(2).find(arg => arg.startsWith('version='));
+
+  if (versionSpecifiedArg) {
+    const version = versionSpecifiedArg.split('=')[1];
+   
+    if (!semver.valid(version)) {
+      console.log('Invalid version specified');
+      process.exit(1);
+    }
+
+    return version;
+  }
+
+  if (patchArg || minorArg || majorArg) {
+    const incrementType = patchArg ? 'patch' : minorArg ? 'minor' : 'major';
+    return semver.inc(package.version, incrementType);
+  }
+
+  console.log('No version specified');
+  process.exit(1);
+}
+
+const changeVersion = (version) => {
+  package.version = version;
+
+  try {
+    // Write new version to package.json
+    fs.writeFileSync(`${process.cwd()}/package.json`, JSON.stringify(package, null, 2));
+  
+    // Write new version to package-lock.json
+    fs.writeFileSync(`${process.cwd()}/package-lock.json`, JSON.stringify(package, null, 2));
+  } catch (error) {
+    console.error('Error writing new version to package.json, either files do not exist or are not writable');
+  }
+  
+  // Modify version in ./helm/values/values.yaml
+  try {
+    const valuesYaml = fs.readFileSync(`${process.cwd()}/helm/values/values.yaml`, 'utf8');
+    const newValuesYaml = valuesYaml.replace(/version:.*/, `version: ${version}`);
+    fs.writeFileSync(`${process.cwd()}/helm/values/values.yaml`, newValuesYaml);
+  } catch (error) {
+    console.error('Error writing to ./helm/values/values.yaml, either file does not exist or is not writable');
+  }
+
+  console.log('Version bumped to', version);
+}
+
+const start = () => {
+  const version = getVersion();
+  if (version) {
+    changeVersion(version);
+  }
+}
+
+start()
